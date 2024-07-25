ae68b5a089b31e15e7f9fb565b0917e27f354531
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-07-25 21:11:19 +0530

    Introduction to Strings

--- Strings/1.cpp
+++ Strings/1.cpp
@@ -6,5 +6,13 @@ int main()
     string name;
     cin >> name;
     cout << "Printing name: " << name << endl;
+
+    int index = 0;
+    while (name[index] != '\0')
+    {
+        cout << "index: " << index << " character: " << name[index] << endl;
+        index++;
+    }
+
     return 0;
 }
