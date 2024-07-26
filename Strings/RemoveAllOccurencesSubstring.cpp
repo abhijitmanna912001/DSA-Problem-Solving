757139d06b3a6a6899d1d34b068462c7b1a483a4
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-07-26 12:44:28 +0530

    Remove All Occurences of a Substring

---
+++ Strings/RemoveAllOccurencesSubstring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+string removeOccurrences(string s, string part)
+{
+    while (s.find(part) != string::npos)
+        s.erase(s.find(part), part.length());
+    return s;
+}
+
+int main()
+{
+
+    return 0;
+}
