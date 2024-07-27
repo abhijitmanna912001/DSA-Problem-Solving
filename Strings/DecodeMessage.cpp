b19a83c41db35fbd72440b6d89c48d768c39a656
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-07-27 20:32:41 +0530

    Decode Message Leetcode Problem

---
+++ Strings/DecodeMessage.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+string decodeMessage(string key, string message)
+{
+    char start = 'a';
+    char mapping[280] = {0};
+
+    for (auto ch : key)
+    {
+        if (ch != ' ' && mapping[ch] == 0)
+        {
+            mapping[ch] = start;
+            start++;
+        }
+    }
+    string ans;
+
+    for (auto ch : message)
+    {
+        if (ch == ' ')
+            ans.push_back(' ');
+        else
+        {
+            char decodedChar = mapping[ch];
+            ans.push_back(decodedChar);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}
