c063f0a76cc40485915f1048fcc40b8258666ae5
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-07-30 13:16:01 +0530

    Valid Anagram Leetcode and GFG Problem

---
+++ Strings/ValidAnagram.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isAnagram(string s, string t)
+{
+    int freqTable[256] = {0};
+    for (int i = 0; i < s.size(); i++)
+        freqTable[s[i]]++;
+    for (int i = 0; i < t.size(); i++)
+        freqTable[t[i]]--;
+    for (int i = 0; i < 256; i++)
+    {
+        if (freqTable[i] != 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+    return 0;
+}
