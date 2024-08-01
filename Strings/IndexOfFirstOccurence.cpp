c437149b522433b3113f33820b962a4b3de4949c
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-01 10:20:54 +0530

    Index Of First Occurence of a String Leetcode and GFG Problem

---
+++ Strings/IndexOfFirstOccurence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int strStr(string haystack, string needle)
+{
+    int n = haystack.size();
+    int m = needle.size();
+
+    for (int i = 0; i <= n - m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (needle[j] != haystack[i + j])
+                break;
+            if (j == m - 1)
+                return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    return 0;
+}
