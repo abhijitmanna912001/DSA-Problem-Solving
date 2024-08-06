45c9b54943a1191aeb2a76354bff6af1cf6ecfb9
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-06 21:50:19 +0530

    GCD Problem

---
+++ Maths/GCD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return 0;
+    if (b == 0)
+        return a;
+
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a == 0 ? b : a;
+}
+
+int main()
+{
+
+    return 0;
+}
