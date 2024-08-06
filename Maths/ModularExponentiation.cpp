ac6b344c5b9e06beabb7e092fa5d22bf37f25b9e
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-06 22:29:47 +0530

    Super Pow Leetcode and ModularExponentiation GFG Problem

---
+++ Maths/ModularExponentiation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+long long int PowMod(long long int x, long long int n, long long int M)
+{
+    long long int ans = 1;
+    while (n > 0)
+    {
+        if (n & 1)
+            ans = (ans * x) % M;
+        x = (x * x) % M;
+        n >>= 1;
+    }
+    return ans % M;
+}
+
+int main()
+{
+
+    return 0;
+}
