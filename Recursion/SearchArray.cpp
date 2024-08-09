27dc03d4308392184cd1dc4437e5725d808ca495
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-09 18:02:43 +0530

    Search an Element in an Array

--- Recursion/SearchArray.cpp
+++ Recursion/SearchArray.cpp
@@ -18,6 +18,12 @@ int search(vector<int> &arr, int x)
 
 int main()
 {
-
+    vector<int> arr = {3, 1, 4, 1, 5, 9, 2};
+    int x = 5;
+    int result = search(arr, x);
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
     return 0;
 }
