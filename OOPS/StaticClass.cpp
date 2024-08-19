744342d68507967f542c547d86397e4e0ad41bc8
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-19 11:25:24 +0530

    Static Class

---
+++ OOPS/StaticClass.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class abc
+{
+public:
+    int x, y;
+    abc() : x(0), y(0) {}
+
+    static void print()
+    {
+        printf("I am a static %s\n ", __FUNCTION__);
+    }
+};
+
+int main()
+{
+    abc obj1;
+    abc::print();
+    abc obj2;
+    abc::print();
+    abc::print();
+    return 0;
+}
