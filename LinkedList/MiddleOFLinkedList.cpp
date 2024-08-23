e41697917bb74feaf64a37914b31327c0ff90fb2
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-08-23 20:45:50 +0530

    Slow and Fast Pointer Method used

--- LinkedList/MiddleOFLinkedList.cpp
+++ LinkedList/MiddleOFLinkedList.cpp
@@ -36,6 +36,23 @@ ListNode *middleNode(ListNode *head)
     return temp;
 }
 
+ListNode *middleNode2(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return slow;
+}
+
 int main()
 {
 
