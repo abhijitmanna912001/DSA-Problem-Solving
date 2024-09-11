f03906c1c89950244d6494076880750018499de7
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-09-11 12:55:13 +0530

    Correction in Base Case

--- DnC&Backtracking/PaintingFence.cpp
+++ DnC&Backtracking/PaintingFence.cpp
@@ -6,7 +6,7 @@ int getPaintWays(int n, int k)
     if (n == 1)
         return k;
     if (n == 2)
-        return k * (k - 1);
+        return k + k * (k - 1);
     int ans = (k - 1) * (getPaintWays(n - 1, k) + getPaintWays(n - 2, k));
     return ans;
 }
