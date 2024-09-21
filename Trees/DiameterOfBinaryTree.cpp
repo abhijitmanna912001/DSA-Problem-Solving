18bcbb5e6ce30911178b396a5814186b4a9cdd30
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-09-21 16:53:48 +0530

    Optimal Way to find Diameter of Binary Tree

--- Trees/DiameterOfBinaryTree.cpp
+++ Trees/DiameterOfBinaryTree.cpp
@@ -14,26 +14,46 @@ struct TreeNode
 class Solution
 {
 public:
+    int D = 0;
     int height(TreeNode *root)
     {
-        if (root == NULL)
+        if (!root)
             return 0;
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        int maxHeight = max(leftHeight, rightHeight) + 1;
-        return maxHeight;
+        int lh = height(root->left);
+        int rh = height(root->right);
+        // int currD = lh + rh + 1; in GFG
+        int currD = lh + rh;
+        D = max(D, currD);
+        return max(lh, rh) + 1;
     }
 
-    int diameterOfBinaryTree(TreeNode *root)
+    int diameterOfBinaryTree2(TreeNode *root)
     {
-        if (root == NULL)
-            return 0;
-        int option1 = diameterOfBinaryTree(root->left);
-        int option2 = diameterOfBinaryTree(root->right);
-        int option3 = height(root->left) + height(root->right) + 1;
-        int diameter = max(option1, max(option2, option3));
-        return diameter;
+        height(root);
+        return D;
     }
+
+    // Brute Force Method
+    // int height(TreeNode *root)
+    // {
+    //     if (root == NULL)
+    //         return 0;
+    //     int leftHeight = height(root->left);
+    //     int rightHeight = height(root->right);
+    //     int maxHeight = max(leftHeight, rightHeight) + 1;
+    //     return maxHeight;
+    // }
+
+    // int diameterOfBinaryTree(TreeNode *root)
+    // {
+    //     if (root == NULL)
+    //         return 0;
+    //     int option1 = diameterOfBinaryTree(root->left);
+    //     int option2 = diameterOfBinaryTree(root->right);
+    //     int option3 = height(root->left) + height(root->right) + 1;
+    //     int diameter = max(option1, max(option2, option3));
+    //     return diameter;
+    // }
 };
 
 int main()
