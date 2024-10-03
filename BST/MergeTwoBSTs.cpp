e0a641cdfb7111673bd936f52a4e0c43388c9d70
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-10-03 14:01:17 +0530

    Merge Two BSTs in Sorted Way GFG Problem

---
+++ BST/MergeTwoBSTs.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> merge(Node *root1, Node *root2)
+    {
+        vector<int> ans;
+        stack<Node *> sa, sb;
+        Node *a = root1, *b = root2;
+
+        while (a || b || !sa.empty() || !sb.empty())
+        {
+            while (a)
+            {
+                sa.push(a);
+                a = a->left;
+            }
+
+            while (b)
+            {
+                sb.push(b);
+                b = b->left;
+            }
+
+            if (sb.empty() || (!sa.empty() && sa.top()->data <= sb.top()->data))
+            {
+                auto atop = sa.top();
+                ans.push_back(atop->data);
+                sa.pop();
+                a = atop->right;
+            }
+            else
+            {
+                auto btop = sb.top();
+                sb.pop();
+                ans.push_back(btop->data);
+                b = btop->right;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
