2d481e35a65b1dd4dadac48f4a332785b7b4a2b2
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-10-08 17:24:20 +0530

    Minimum Number of Refueling Stops LeetCode Problem

---
+++ Heaps/MinimumNumberOfRefuellingStops.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>> &stations)
+    {
+        int stops = 0, dist = 0;
+        int i = 0, fuel = startFuel;
+        priority_queue<pair<int, int>> pq;
+        while (true)
+        {
+            while (i < stations.size())
+            {
+                if (stations[i][0] <= dist + fuel)
+                    pq.push({stations[i][1], stations[i][0]});
+                else
+                    break;
+                ++i;
+            }
+
+            dist += fuel;
+            fuel = 0;
+
+            if (dist >= target)
+                break;
+
+            if (pq.empty())
+            {
+                stops = -1;
+                break;
+            }
+
+            auto &top = pq.top();
+            fuel = dist - top.second + top.first;
+            dist = top.second;
+            pq.pop();
+            ++stops;
+        }
+
+        return stops;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
