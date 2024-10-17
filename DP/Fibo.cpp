6090689e24f0e4929e6e756ccb2ea2c34d736183
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-10-17 17:13:24 +0530

    Fibonacci Number Using Two Methods Of Approaching DP LeetCode Problem

--- DP/Fibo.cpp
+++ DP/Fibo.cpp
@@ -5,24 +5,45 @@ using namespace std;
 class Solution
 {
 public:
-    int solveUsingMemo(int n, vector<int> &dp)
+    // 2. Bottom Up Approach
+    int solveUsingTabulation(int n)
     {
-        if (n == 0 || n == 1)
-            return n;
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        if (n == 0)
+            return dp[0];
 
-        if (dp[n] != -1)
-            return dp[n];
-
-        dp[n] = solveUsingMemo(n - 1, dp) + solveUsingMemo(n - 2, dp);
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         return dp[n];
     }
 
     int fib(int n)
     {
-        vector<int> dp(n + 1, -1);
-        int ans = solveUsingMemo(n, dp);
+        int ans = solveUsingTabulation(n);
         return ans;
     }
+
+    // 1. Top-down Approach
+    // int solveUsingMemo(int n, vector<int> &dp)
+    // {
+    //     if (n == 0 || n == 1)
+    //         return n;
+
+    //     if (dp[n] != -1)
+    //         return dp[n];
+
+    //     dp[n] = solveUsingMemo(n - 1, dp) + solveUsingMemo(n - 2, dp);
+    //     return dp[n];
+    // }
+
+    // int fib(int n)
+    // {
+    //     vector<int> dp(n + 1, -1);
+    //     int ans = solveUsingMemo(n, dp);
+    //     return ans;
+    // }
 };
 
 int main()
