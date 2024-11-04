ab289d9e50bc1d07993ae150927cee8c53981f15
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-11-04 10:42:50 +0530

    Stone Game LeetCode Problem

---
+++ DP/StoneGame.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool stoneGame(vector<int> &piles)
+    {
+        return true;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
