0287363dfca9e1c58f365404fb844c0ee6335105
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-11-05 19:29:23 +0530

    Minimum Insertion Steps to Make a String Palindrome LeetCode and GFG Problem

---
+++ DP/MinimumInsertStepsToMakeStringPalindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int solveUsingSpaceOptimised(string a, string b)
+    {
+        vector<int> curr(a.length() + 1, 0);
+        vector<int> next(a.length() + 1, 0);
+
+        for (int j_idx = b.length() - 1; j_idx >= 0; j_idx--)
+        {
+            for (int i_idx = a.length() - 1; i_idx >= 0; i_idx--)
+            {
+                int ans = 0;
+                if (a[i_idx] == b[j_idx])
+                    ans = 1 + next[i_idx + 1];
+                else
+                    ans = max(next[i_idx], curr[i_idx + 1]);
+                curr[i_idx] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
+    int longestPalindromeSubseq(string &s)
+    {
+        string a = s;
+        reverse(a.begin(), a.end());
+        int ans = solveUsingSpaceOptimised(a, s);
+        return ans;
+    }
+
+    int minInsertions(string s)
+    {
+        int LPS_len = longestPalindromeSubseq(s);
+        int insertions = s.size() - LPS_len;
+        return insertions;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
