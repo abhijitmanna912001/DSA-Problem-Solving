e0c1160cd69d663b0da88c86eee4572b32586ce1
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-11-15 23:39:03 +0530

    Course Schedule II LeetCode and GFG Problem

---
+++ Graphs/CourseScheduleII.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    void topoSortBFS(int n, vector<int> &topoOrder, unordered_map<int, list<int>> &adjList)
+    {
+        queue<int> q;
+        unordered_map<int, int> indegree;
+        for (auto i : adjList)
+        {
+            for (auto nbr : i.second)
+                indegree[nbr]++;
+        }
+
+        for (int node = 0; node < n; node++)
+        {
+            if (indegree[node] == 0)
+                q.push(node);
+        }
+
+        while (!q.empty())
+        {
+            int frontNode = q.front();
+            q.pop();
+            topoOrder.push_back(frontNode);
+
+            for (auto nbr : adjList[frontNode])
+            {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0)
+                    q.push(nbr);
+            }
+        }
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        unordered_map<int, list<int>> adjList;
+        for (vector<int> i : prerequisites)
+        {
+            int u = i[0];
+            int v = i[1];
+            adjList[v].push_back(u);
+        }
+
+        vector<int> topoSort;
+        topoSortBFS(numCourses, topoSort, adjList);
+        if (topoSort.size() == numCourses)
+            return topoSort;
+        else
+            return {};
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
