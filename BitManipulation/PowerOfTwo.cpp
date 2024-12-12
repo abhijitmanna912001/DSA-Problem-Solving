9661e3c2182fa69c96fc92da650b2c54a6d7143f
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2024-12-12 13:14:18 +0530

    Power of 2 LeetCode and GFG Problem

---
+++ BitManipulation/PowerOfTwo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
