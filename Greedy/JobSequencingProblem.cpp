3a1ba777fc1351fc91ebda065168ff96ee9757ca
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-03-29 11:15:17 +0530

    New DSU Method Solution Added

--- Greedy/JobSequencingProblem.cpp
+++ Greedy/JobSequencingProblem.cpp
@@ -13,6 +13,46 @@ struct Job
 class Solution
 {
 public:
+    // M2- DSU NLogN complexity solution
+    int findParent(int slot, vector<int>& parent) {
+        if(parent[slot] == slot)
+            return slot;
+            
+        return parent[slot] = findParent(parent[slot], parent);
+    }
+    
+    vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
+        int n = deadline.size();
+        vector<pair<int, int>> jobs;
+        
+        for(int i = 0; i < n; i++)
+            jobs.push_back({profit[i], deadline[i]});
+            
+        sort(jobs.rbegin(), jobs.rend());
+        
+        int maxDeadline = 0;
+        for(int i = 0; i < n; i++)
+            maxDeadline = max(maxDeadline, jobs[i].second);
+            
+        vector<int> parent(maxDeadline + 1);
+        for(int i = 0; i <= maxDeadline; i++)
+            parent[i] = i;
+            
+        int count = 0, maxProfit = 0;
+        for(int i = 0; i < n; i++) {
+            int currProfit = jobs[i].first;
+            int currDeadline = jobs[i].second;
+            
+            int availableSlot = findParent(currDeadline, parent);
+            if(availableSlot > 0) {
+                count++;
+                maxProfit += currProfit;
+                parent[availableSlot] = findParent(availableSlot - 1, parent);
+            }
+        }
+        return {count, maxProfit};
+    }
+
     static bool cmp(Job a, Job b)
     {
         return a.profit > b.profit;
